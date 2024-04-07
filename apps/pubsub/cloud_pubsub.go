fcdb21d484323ab7ecda24c4abfc927d34b5f30a
alaraiabdiallah <alaraiabdiallah@gmail.com>
2024-04-07 21:58:55 +0700

    feat: prototype of pubsub implementation

--- apps/pubsub/cloud_pubsub.go
+++ apps/pubsub/cloud_pubsub.go
@@ -1,23 +1,127 @@
 package pubsub
 
-import "gojumpstart/core/service"
+import (
+	"context"
+	"fmt"
+	"gojumpstart/core/service"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	cloud_pubsub "cloud.google.com/go/pubsub"
+	"go.uber.org/zap"
+	"google.golang.org/api/option"
+)
 
 type PubSubApp struct {
 	// cloud pubsub client
-	// Client *pubsub.Client
+	client *cloud_pubsub.Client
+	// project id
+	projectID string
+	// serviceAccountFile
+	serviceAccountFile string
+	// zap logger
+	logger *zap.Logger
+	// Context
+	ctx context.Context
 	// core service
 	Svc *service.Service
 	// handler
 }
 
 func NewPubSubApp(service *service.Service) *PubSubApp {
+	appEnv := os.Getenv("APP_ENV")
+	logger, _ := zap.NewDevelopment()
+	if appEnv == "prod" {
+		logger, _ = zap.NewProduction()
+	}
 	return &PubSubApp{
-		Svc: service,
+		Svc:    service,
+		logger: logger,
+	}
+}
+
+func (p *PubSubApp) initEnv() {
+	p.ctx = context.Background()
+	p.projectID = os.Getenv("GCP_PROJECT_ID")
+	p.serviceAccountFile = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+	if p.projectID == "" {
+		p.logger.Panic("GCP_PROJECT_ID is required")
+	}
+}
+
+func (p *PubSubApp) initClient() {
+	var client *cloud_pubsub.Client
+	var err error
+
+	if p.client != nil {
+		return
+	}
+	if p.serviceAccountFile == "" {
+		client, err = cloud_pubsub.NewClient(p.ctx, p.projectID)
+		if err != nil {
+			p.logger.Panic("Error creating pubsub client", zap.Error(err))
+		}
+	} else {
+		client, err = cloud_pubsub.NewClient(p.ctx, p.projectID, option.WithCredentialsFile(p.serviceAccountFile))
+		if err != nil {
+			p.logger.Panic("Error creating pubsub client", zap.Error(err))
+		}
+	}
+	p.client = client
+
+}
+
+func (p *PubSubApp) subscribe() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(p *PubSubApp, wg *sync.WaitGroup) {
+		defer wg.Done()
+		// Create a new subscription with the given name.
+		cctx, cancel := context.WithCancel(p.ctx)
+		defer cancel()
+		sub1 := p.client.Subscription("first-sub")
+		err := sub1.Receive(cctx, func(ctx context.Context, msg *cloud_pubsub.Message) {
+			isoDateTime := time.Now().Format(time.RFC3339)
+			fmt.Printf("(%v)[first-sub] Got message: %q\n", isoDateTime, string(msg.Data))
+			msg.Ack()
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(p, &wg)
+	wg.Add(1)
+	go func(p *PubSubApp, wg *sync.WaitGroup) {
+		wg.Add(1)
+		defer wg.Done()
+		// Create a new subscription with the given name.
+		cctx, cancel := context.WithCancel(p.ctx)
+		defer cancel()
+		sub2 := p.client.Subscription("second-sub")
+		err := sub2.Receive(cctx, func(ctx context.Context, msg *cloud_pubsub.Message) {
+			var isoDateTime string = time.Now().Format(time.RFC3339)
+			fmt.Printf("(%v)[second-sub] Got message: %q\n", isoDateTime, string(msg.Data))
+			msg.Ack()
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(p, &wg)
+
+	wg.Wait()
+}
+
+func (p *PubSubApp) close() {
+	if p.client != nil {
+		p.client.Close()
 	}
 }
 
 func (p *PubSubApp) Run() {
-	// p.Client = pubsub.NewClient(context.Background(), "project-id")
-	// defer p.Client.Close()
-	// p.subscribe()
+	p.initEnv()
+	p.initClient()
+	defer p.close()
+	p.subscribe()
 }
